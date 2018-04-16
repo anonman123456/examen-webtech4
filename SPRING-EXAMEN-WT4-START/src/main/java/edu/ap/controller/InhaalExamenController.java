d57794737677c8112bfb441286b011bf7574c71f
Tom De Backer <tom.debacker02@student.ap.be>
2018-04-16 15:05:21 +0200

    vraag 1 (deelvraag 2 get)

--- SPRING-EXAMEN-WT4-START/src/main/java/edu/ap/controller/InhaalExamenController.java
+++ SPRING-EXAMEN-WT4-START/src/main/java/edu/ap/controller/InhaalExamenController.java
@@ -53,4 +53,39 @@ public class InhaalExamenController {
 			return ResponseEntity.ok(HttpStatus.I_AM_A_TEAPOT);
 		}
 	}
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	@ResponseBody
+	public String listAllInhaalExamenForStudent(@RequestParam("student") String student) {
+		Set<String> keysOfStudent = service.keys(student + "*");
+		// get data
+		ArrayList<InhaalExamen> examens = new ArrayList<>();
+		for (String keyOfStudent : keysOfStudent) {
+			Map<Object, Object> entry = service.hgetAll(keyOfStudent);
+			examens.add(new InhaalExamen((String) entry.get("student"), (String) entry.get("exam"),
+					(String) entry.get("reason"), (String) entry.get("date")));
+		}
+
+		// sort by reason
+		Collections.sort(examens , new CustomComparator());
+		
+		
+		String html = "<HTML><BODY><h3> Aanvragen inhaalexamen van " + student + "</h3>";
+		for (InhaalExamen examen : examens) {
+			html +=  "<div><h5>Examen: " + examen.getExam() + "</h5>";
+			html +=  "<p>Reden: " + examen.getReason() + "</p>";
+			html +=  "<p>Datum: " + examen.getDate() + "</p>";
+			html += "</div>";
+			html += "<br>";
+		}
+		html += "</BODY></HTML>";
+		return html;
+	}
+	
+	private class CustomComparator implements Comparator<InhaalExamen> {
+	    @Override
+	    public int compare(InhaalExamen o1, InhaalExamen o2) {
+	        return o1.getExam().compareTo(o2.getExam());
+	    }
+	}
 }
